e5daa815b89bfbbb91a4963943fd54be46688a3a
niklasbec <niklasbeckerr@gmail.com>
2020-01-16 10:48:31 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+
+const express = require('express')
+const cors = require('cors')
+const helmet = require('helmet')
+
+const port = process.env.PORT || 4000
+
+const app = express()
+
+app.use(helmet())
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/greeting', (req, res) => {
+  res.json(`Hello, ${req.query.name}! How are you?`)
+})
+
+app.use((req, res) => {
+  res.json('This API is alive!')
+})
+
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
+})
