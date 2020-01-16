55e1c538484a7ff356bf736909899264b2fe00d8
niklasbec <niklasbeckerr@gmail.com>
2020-01-16 11:54:30 +0100

    hitting my own endpoint

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [greeting, setGreeting] = useState('')
+
+  useEffect(() => {
+    fetch('/api/greeting?name=nick')
+      .then(res => res.json())
+      .then(greeting => setGreeting(greeting))
+      .catch(err => {
+        debugger
+      })
+    })
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        { greeting }
       </header>
     </div>
   );
